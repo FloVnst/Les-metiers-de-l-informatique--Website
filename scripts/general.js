520f7ccb132a762a47660b0b660a90b9b6ea66cc
Vaneste Florian <florian_vaneste@ens.univ-artois.fr>
2020-06-09 03:21:35 +0200

    internet explorer disclaimer

---
+++ scripts/general.js
@@ -0,0 +1,12 @@
+// Internet Explorer disclaimer
+function internetExplorerDisclaimer() {
+
+    var browser = window.navigator.userAgent.indexOf("MSIE ");
+
+    if (browser > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        alert("Ce site n'est pas pris en charge par Internet Explorer. Nous vous conseillons d'utiliser un navigateur plus récent tel que Mozilla Firefox, Brave ou Chromium.");
+    }
+}
+
+window.addEventListener("load", internetExplorerDisclaimer)
